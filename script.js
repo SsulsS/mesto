6c45de8392bf18743c21d115efec9c5c22e3c091
SsulsS <114453034+SsulsS@users.noreply.github.com>
2023-01-15 20:24:39 +0300

    Отправка

--- script.js
+++ script.js
@@ -1,22 +1,22 @@
 let editButton = document.querySelector(".profile__edit-button");
-let saveButton = document.querySelector(".form__button");
+let saveButton = document.querySelector(".edit-form__button");
 let editForm = document.querySelector(".edit-form");
-let closeButton = document.querySelector(".form__close-button");
+let closeButton = document.querySelector(".edit-form__close-button");
 let profileInfo = document.querySelector(".profile__info")
 let profileName = document.querySelector(".profile__name")
 let profileActivity = document.querySelector(".profile__activity")
 
 function openForm(){
     editForm.classList.remove('edit-form_disabled');
-    let person = document.querySelector(".form__input_type_name");
-    let activity = document.querySelector(".form__input_type_activity");
+    let person = document.querySelector(".edit-form__input_type_name");
+    let activity = document.querySelector(".edit-form__input_type_activity");
     person.value = profileName.textContent;
     activity.value = profileActivity.textContent;
 }
 
 function save(){
-    let person = document.querySelector(".form__input_type_name");
-    let activity = document.querySelector(".form__input_type_activity");
+    let person = document.querySelector(".edit-form__input_type_name");
+    let activity = document.querySelector(".edit-form__input_type_activity");
 
 
 
