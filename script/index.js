6786a35d1b16540ffa3de4317bc8f047f97b6d67
SsulsS <114453034+SsulsS@users.noreply.github.com>
2023-01-27 06:25:01 +0300

    Правки Js Отступы 320px element

--- script/index.js
+++ script/index.js
@@ -1,34 +1,31 @@
 let editButton = document.querySelector('.profile__edit-button');
-let saveButton = document.querySelector('.edit-form__button');
-let editForm = document.querySelector('.edit-form');
-let closeButton = document.querySelector('.edit-form__close-button');
+let editForm = document.querySelector('.popup');
+let closeButton = document.querySelector('.popup__close-button');
 let profileInfo = document.querySelector('.profile__info')
 let profileName = document.querySelector('.profile__name')
 let profileActivity = document.querySelector('.profile__activity')
+let person = document.querySelector('.popup__input_type_name');
+let activity = document.querySelector('.popup__input_type_activity');
+let saveButton = document.querySelector('.popup__button');
 
-function openForm(){
-    editForm.classList.remove('edit-form_opened');
-    let person = document.querySelector('.edit-form__input_type_name');
-    let activity = document.querySelector('.edit-form__input_type_activity');
+function close() {
+    editForm.classList.add('popup_opened');
+}
+
+function openPopup(){
+    editForm.classList.remove('popup_opened');
     person.value = profileName.textContent;
     activity.value = profileActivity.textContent;
 }
 
-function save(){
-    let person = document.querySelector('.edit-form__input_type_name');
-    let activity = document.querySelector('.edit-form__input_type_activity');
-
-
-
+function save(event){
+    event.preventDefault();
     profileName.textContent = person.value;
     profileActivity.textContent = activity.value;
-    editForm.classList.add('edit-form_opened');
+    close()
 }
 
-function close() {
-    editForm.classList.add('edit-form_opened');
-}
-
-editButton.addEventListener('click', openForm);
-saveButton.addEventListener('click', save);
+editButton.addEventListener('click', openPopup);
 closeButton.addEventListener('click', close);
+saveButton.addEventListener('click', save);
+/*Не понял как передать значение чере submit, Оставил ивент что бы кнопка работала, если сможете подсказать поподробнее буду благодарен */
